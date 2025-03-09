fe8c6ee3c581ced7c9ba8502f897e093fb3a08a2
hennyrose <93723100+hennyrose@users.noreply.github.com>
2025-03-09 23:45:38 +0200

    aws-ready-more-4

--- src/main/java/com/game/cyberslots/HealthController.java
+++
@@ -1,9 +0,0 @@
-package com.game.cyberslots;
-
-@RestController
-public class HealthController {
-    @GetMapping("/actuator/health")
-    public ResponseEntity<Map<String, String>> health() {
-        return ResponseEntity.ok(Map.of("status", "UP"));
-    }
-}
